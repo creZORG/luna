65e635de31157a956d918faf5f4ff433b77ea145
tensorg gl <gg.tensor@gmail.com>
2025-09-23 02:39:34 +0000

    use these three images in homepage https://i.postimg.cc/CL1z03NN/Gemini-

--- next.config.ts
+++ next.config.ts
@@ -38,6 +38,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "firebasestorage.googleapis.com",
       },
+      {
+        protocol: "https",
+        hostname: "i.postimg.cc",
+      }
     ],
   },
 };
