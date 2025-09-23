55385b287afbaf1d574e331f8e60156b03a92c1b
tensorg gl <gg.tensor@gmail.com>
2025-09-23 02:22:28 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,15 @@
+
+import { initializeApp, getApps, getApp } from 'firebase/app';
+
+const firebaseConfig = {
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export { app };
